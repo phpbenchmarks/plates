c3ebfa00a23c28bf5325fc664513e414643de75a
Steevan BARBOYON <steevan.barboyon@gmail.com>
2018-04-22 11:25:51 +0200

    Use new repositories structure

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
 
-require_once('../vendor/phpbenchmarks/plates/helloworld.php');
+require_once('../vendor/phpbenchmarks/plates-common/helloworld.php');
 
 // require phpbenchmarks stats.php here when needed
